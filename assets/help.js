37bc28a6f0dbf6cbe5369a9908511eac70e3b4e1
notoliver <47568829+notoliver@users.noreply.github.com>
2021-11-22 19:41:02 -0500

    a

--- assets/help.js
+++ assets/help.js
@@ -1,15 +1,19 @@
 var gifs = ['size', 'angle', 'spacing', 'aspectratio', 'force', 'hardness'];
+var usePopup = true;
+var useFixed = false;
 
 function loadGifs(){
-    var temptext = ''
+    var temptext1 = ''
+    var temptext2 = ''
     for(var i=0;i<gifs.length;i++){
-       temptext+='<div class=\'gif\' id=\''+gifs[i]+'\' style=\'display:none\'>\r\n\t\t\t<img src=\'assets\/'+gifs[i]+'.gif\'\/>\r\n\t\t<\/div>'
+       temptext1+='<div class=\'gif\' id=\''+gifs[i]+'\' style=\'display:none\'>\r\n\t\t\t<img src=\'assets\/'+gifs[i]+'.gif\'\/>\r\n\t\t<\/div>'
+       temptext2+='<div class=\'fgif\' id=\'f'+gifs[i]+'\' style=\'display:none\'>\r\n\t\t\t<img src=\'assets\/'+gifs[i]+'.gif\'\/>\r\n\t\t<\/div>'
     }
-    document.getElementById('gifs').innerHTML+=temptext;
+    document.getElementById('popupgifs').innerHTML+=temptext1;
+    document.getElementById('fixedgifs').innerHTML+=temptext2;
 }
 
 loadGifs();
-
 
 function getMousePos(e) {
     return {x:e.clientX,y:e.clientY};
@@ -17,21 +21,29 @@ function getMousePos(e) {
 
 function showBox(id,x,y){
     //console.log(document.getElementById(id));
-    document.getElementById(id).style.top = y+'px';
-    document.getElementById(id).style.left = x+'px';
-    document.getElementById(id).style.position = 'absolute';
-    document.getElementById(id).style.display = 'block';
+    if(usePopup){
+        document.getElementById(id).style.top = y+'px';
+        document.getElementById(id).style.left = x+'px';
+        document.getElementById(id).style.position = 'absolute';
+        document.getElementById(id).style.display = 'block';
+    }
+    if(useFixed){
+        document.getElementById('f'+id).style.top = 900+'px';
+        document.getElementById('f'+id).style.left = 0+'px';
+        document.getElementById('f'+id).style.position = 'absolute';
+        document.getElementById('f'+id).style.display = 'block';
+    }
 }
-
 
 document.onmousemove=function(e) {
     var mousecoords = getMousePos(e);
     var mouseX = mousecoords.x;
     var mouseY = mousecoords.y;
-    //document.getElementById('temptext').innerHTML=mouseX + ','+mouseY;
+    //document.getElementById('installpopup').innerHTML=mouseX + ','+mouseY;
 
     for(var i=0;i<gifs.length;i++){
         document.getElementsByClassName('gif')[i].style.display='none';
+        document.getElementsByClassName('fgif')[i].style.display='none';
     }
 
     if(mouseX>2 && mouseX<185){
@@ -55,5 +67,3 @@ document.onmousemove=function(e) {
         }
     }
 };
-
-
